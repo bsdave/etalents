e09ceac5cf17a032b3f6996c3a769034e85a4f7f
Kirill Maysner <kirillmajsner@gmail.com>
2019-06-26 14:40:08 +0600

    кнопка ебучая

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -76,23 +76,16 @@ $(function () {
 
 const startCounting = () => {
   $('.amount').each(function () {
-    var $this = $(this),
-        countTo = $this.attr('data-count');
-
-    $({countNum: $this.text()}).animate({
-          countNum: countTo
-        },
-        {
-          duration: 5000,
-          easing: 'linear',
-          step: function () {
-            $this.text(Math.floor(this.countNum));
-          },
-          complete: function () {
-            $this.text(this.countNum);
-          }
-        });
-  });
+    $(this).prop('Counter', 0).animate({
+      Counter: $(this).text()
+    }, {
+      duration: 5000,
+      easing: 'swing',
+      step: function (now) {
+        $(this).text(Math.ceil(now))
+      }
+    })
+  })
 };
 
 //Wizard
